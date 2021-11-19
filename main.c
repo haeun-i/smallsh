0f8ee05e4ec6ff31578bf6496a4022e43b2a5ee7
Haeun Kim <gkdms1120s@gmail.com>
2021-11-19 19:59:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "smallsh.h"
 
-char *prompt = "Command> ";
+char *prompt = "Command> "; // 명령어 입력을 위해 띄워놓는 문장
 
 int main() {
-	while(userin(prompt) != EOF)
-		procline();
+	while(userin(prompt) != EOF) // EOF이 아니면
+		procline(); // procline 실행 (parsing해서 실제 응용프로그램 실행)
 	return 0;
 }
